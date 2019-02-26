c237c6196f2dda532a0170685cb5a1f2a0afff10
Francis Victa <francisvicta45@gmail.com>
2019-02-25 21:57:27 -0500

    Added functional login page
    added login page, placeholder home pages, made changes to database content

---
+++ TechKnowPro/HomeCustomer.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TechKnowPro.Model;
+
+
+namespace TechKnowPro
+{
+    public partial class HomeCustomer : System.Web.UI.Page
+    {
+        User user;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //no login info
+            if (Session["user"] == null) { Response.Redirect("~/Login.aspx"); }
+            //get user information
+            user = (User)Session["user"];
+            //if not a technician, redirect to login page
+            if (user.role != "customer") { Response.Redirect("~/Login.aspx"); }
+        }
+    }
+}
---
+++ TechKnowPro/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechKnowPro.Model
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string username { get; set; }
+        public string role { get; set; }
+    }
+}
